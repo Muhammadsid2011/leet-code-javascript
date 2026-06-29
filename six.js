23a13636118bf5cbc83745088d36b10db3ad49c8
Muhammadsid2011 <muhammadsid2011@gmail.com>
2026-06-29 22:59:02 +0500

    Complete sixth leet code problem

---
+++ six.js
@@ -0,0 +1,16 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+function twoSum(nums, target) {
+    let ansArray = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                ansArray.push(i, j);
+                return ansArray;
+            }
+        }
+    }
+}
+console.log(twoSum([2,7,11,15], 9))
